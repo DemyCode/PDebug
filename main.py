4c0e032c6e582d48abe2c387cdbae78079d9ba62
DemyCode <mehdi.bekhtaoui@epita.fr>
2021-04-04 14:50:13 +0200

    [FEAT] Added time consumption in tree

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import sys
 
 from tracer import Tracer
 
+class Fraction:
+    def __init__(self, num, denum):
+        self.num = num
+        self.denum = denum
+
+    def __add__(self, rhs):
+        return Fraction(self.num * rhs.denum + rhs.num * self.denum, self.denum * rhs.denum)
+
+    def __str__(self):
+        return '({}/{})'.format(self.num, self.denum)
+
+def adder(a, b, c, d):
+    return Fraction(a, b) + Fraction(c, d)
+
 
 def quicksort(l):
     if len(l) <= 1:
@@ -15,17 +29,19 @@ def sub_sub_main():
     return 2
 
 
-def sub_main():
+def sub_main(a, b):
     return sub_sub_main()
 
-
 def main():
-    quicksort([2, 4, 0, 23, -8, 4])
+    print(adder(1, 2, 1, 2))
+    print(quicksort([0, 12, -5, 6, 24, 3, 1]))
 
+import time
 
 if __name__ == '__main__':
     tracer = Tracer()
     tracer.activate()
+    main()
     main()
     tracer.deactivate()
     tracer.tree.prettyprint()
--- tracer.py
+++ tracer.py
@@ -1,40 +1,60 @@
 import sys
-
+import json
+import time
 
 class Tree:
-    def __init__(self, value: str = None, children=None):
+    def __init__(self, value = None, children = None):
         self.value = value
         if children is None:
             self.children = []
 
     def insert(self, value, n):
-        if n == 0:
+        if n <= 0:
             self.children.append(Tree(value=value))
+            return self.children[-1]
         else:
-            self.children[-1].insert(value, n - 1)
+            return self.children[-1].insert(value, n - 1)
 
     def prettyprint(self, spacing=0):
-        print(' ' * spacing + self.value)
+        print('  ' * spacing + str(self.value))
         for child in self.children:
             child.prettyprint(spacing=spacing + 1)
-
+        
+    def serialize(self):
+        # TODO
+        pass
 
 class Tracer:
     def __init__(self):
-        self.tree = Tree('activate')
+        self.tree = Tree({
+            'function' : 'activate',
+            'parameters' : [],
+            'parameters_values' : [],
+            'return_values' : []
+        })
         self.deepness = 0
         self.tracefunc = sys.gettrace()
+        self.currenttree = []
 
     def activate(self):
         def printer(frame, event, args):
-            # print(self.deepness)
-
+            
             if event == 'call':
-                self.tree.insert(value=frame.f_code.co_name, n=self.deepness)
+                value = {
+                    'function' : frame.f_code.co_name,
+                    'parameters' : [frame.f_code.co_varnames for i in range(frame.f_code.co_argcount)],
+                    'parameters_values' : [frame.f_locals[frame.f_code.co_varnames[i]] for i in range(frame.f_code.co_argcount)],
+                    'return_values' : [],
+                    'time' : time.time()
+                }
+                self.currenttree.append(self.tree.insert(value=value, n=self.deepness))
                 self.deepness += 1
 
-            if event == 'return':
+            elif event == 'return':
                 self.deepness -= 1
+                self.currenttree[-1].value['time'] = time.time() - self.currenttree[-1].value['time']
+                self.currenttree[-1].value['return_values'] = args
+                self.currenttree.pop(-1)
 
             return printer
 
@@ -42,3 +62,9 @@ class Tracer:
 
     def deactivate(self):
         sys.settrace(self.tracefunc)
+    
+    def prettyprint(self):
+        self.tree.prettyprint()
+
+    def serialize(self):
+        self.tree.serialize()
